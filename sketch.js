1ca2d5127484bb123adadad8ba2a0ad12d0ed7a5
aruldhinakar <95573397+aruldhinakar@users.noreply.github.com>
2021-12-12 12:27:09 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,20 +1,18 @@
 var box;
 function setup() {
   createCanvas(400,400);
-box = createSprite(200,200,20,20)
-
+box = createSprite(200,200,20,20);
+background("black");
 }
 
 function draw() 
 {
-  background("black");
 if (keyDown("right"))
 {
-  box.x = box.x +5;
+  box.x = box.x + 2;
+  background("red");
 }
-
 drawSprites();
-
 }
 
 
